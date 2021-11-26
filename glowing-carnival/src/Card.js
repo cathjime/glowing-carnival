99bea97292dc494a48bd09e869cef8965fb4be8d
cathjime <xcatherine.jimenez@gmail.com>
2021-11-26 15:23:04 -0500

    mapping over data to print cards to screen

---
+++ glowing-carnival/src/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = (props) => {
+  const { title, img, description, rating, price } = props;
+  console.log("Props: ", props);
+  return (
+    <div>
+      <h4>{title}</h4>;
+      <img
+        // onClick={this.beyImageClickHandler}
+        alt=""
+        src={img}
+      />
+      <h4>{description}</h4>
+      <h4>{rating} stars</h4>
+      <h4> ${price} / night</h4>
+    </div>
+  );
+};
+
+export default Card;
--- glowing-carnival/src/CardsContainer.js
+++ glowing-carnival/src/CardsContainer.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Card from "./Card";
+import data from "./data";
 
 const CardsContainer = () => {
-  return <h4>This will spit out all the cards</h4>;
+  const dataMap = data.map((dataItem) => (
+    <Card
+      key={dataItem.id}
+      title={dataItem.title}
+      img={dataItem.img}
+      description={dataItem.description}
+      rating={dataItem.rating}
+      price={dataItem.price}
+    />
+  ));
+
+  return (
+    <>
+      <h4>This will spit out all the cards</h4>, {dataMap}
+    </>
+  );
 };
 
 export default CardsContainer;
+
+//  Notes
+//  how to deconstruct dataObj within the map function
