6f92b54e8c51cfd8676a6537f02d51e906cc8cd3
cathjime <xcatherine.jimenez@gmail.com>
2021-12-12 10:32:09 -0500

    filter by rating feature done

--- glowing-carnival/src/Home.js
+++ glowing-carnival/src/Home.js
@@ -4,6 +4,7 @@ import SearchIcon from "@material-ui/icons/Search"; //  add into searchBar later
 import { Select, MenuItem, FormControl, InputLabel } from "@material-ui/core";
 import "./Home.css";
 import data from "./data";
+import { ContactSupportOutlined } from "@material-ui/icons";
 //  install esLint
 //  make sure to order imports correctly
 
@@ -14,7 +15,7 @@ const Home = () => {
 
   const changeHandler = (e, cardId, dataArray) => {
     const val = e.target.value;
-    setStarRating(val);
+    setStarRating(val); //   when this is commented out, rating no longer renders the updated val on card. why?
     updateStarRating(cardId, dataArray, val);
   };
 
@@ -55,25 +56,73 @@ const Home = () => {
     setListingsData(filteredItems);
   };
 
+  const filterByRating = (ratingVal) => {
+    console.log(ratingVal);
+    const filteredRatings = listingsData.filter((dataItem) => {
+      return dataItem.rating >= ratingVal;
+    });
+    setListingsData(filteredRatings);
+  };
+
+  console.log("data: ", listingsData);
   return (
     <>
       <h1>HOME</h1>,<h2>This is the header component</h2>,
-      <h2>Here's a banner now</h2>
+      <h2>Here's a banner </h2>
       <SearchBar
         onChange={(newValue) => setSearchTerm(newValue)}
         onRequestSearch={() => requestSearch(searchTerm)}
         placeholder="Search for a location"
       />
-      <div className="ratingFilter">
+      <div className="ratingsFilter">
         {/* should also have an onClick to return stars with x rating
-        1 star and up / 1 star only
-        2 stars and up / 2 stars only
-        3 stars and up / 3 stars only
-        4 stars and up  / 4 stars only
-        5 stars and up / 5 stars only
-        drop down with radio button when button is selected then saec button on bottom is clicked render new data
-        */}
-        <h5>Search by Star Rating</h5>
+  drop down with radio button when button is selected then saec button on bottom is clicked render new data
+  */}
+        <h5>Search by Star Rating:</h5>
+        <form>
+          <input
+            type="radio"
+            value="1"
+            id="1"
+            onChange={(e) => filterByRating(e.target.value)}
+            name="1 star"
+          />
+          <label for="1 star">1 Star and Up</label>
+          <input
+            type="radio"
+            value="2"
+            id="2"
+            onChange={(e) => filterByRating(e.target.value)}
+            name="2 stars"
+          />
+          <label for="2 stars">2 Stars and Up</label>
+          <input
+            type="radio"
+            value="3"
+            id="3"
+            onChange={(e) => filterByRating(e.target.value)}
+            name="3 stars"
+          />
+          <label for="3 stars">3 Stars and Up</label>
+          <input
+            type="radio"
+            value="4"
+            id="4"
+            onChange={(e) => filterByRating(e.target.value)}
+            name="4 stars"
+          />
+          <label for="4 stars">4 Stars and Up</label>
+          <input
+            type="radio"
+            value="5"
+            id="5"
+            onChange={(e) => filterByRating(e.target.value)}
+            name="5 stars"
+          />
+          <label for="5 stars">5 Stars and Up</label>
+        </form>
+
+        <p>Your selection is {starRating}</p>
       </div>
       <p>TEST SELECTED: {starRating}</p>
       {dataMap}
@@ -87,5 +136,6 @@ export default Home;
 //  you don't need a value within a material-ui searchbar
 //  material-ui select requires the e passed into the (e) => function(e), otherwise it won't work
 //  material-ui card img won't show unless there's a height defined
-//  how to deconstruct dataObj within the map function
+//  how to deconstruct dataObj within the map function (you can't)
 //  thinking about component setup when setting up the filtering for the data
+//  making the data persist - currently it doesn't persist after refresh
